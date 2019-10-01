926bcc865c5ff3d1c66cda4b9f71f1eafdb69632
Saeed Murrad <smurad@pionlog.com>
2019-10-01 18:36:21 +0500

    Added End Points for Student and Class operations.

--- src/main/java/com/student/info/sys/controller/StudentController.java
+++ src/main/java/com/student/info/sys/controller/StudentController.java
@@ -1,30 +1,111 @@
 package com.student.info.sys.controller;
 
+import com.student.info.sys.exception.ResourceNotFoundException;
+import com.student.info.sys.model.Classes;
 import com.student.info.sys.model.Student;
+import com.student.info.sys.repository.ClassRepository;
 import com.student.info.sys.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Created By Saeed Murrad 10/01/2019
+ */
 
 @RestController
+@RequestMapping(value = "/")
 public class StudentController {
 
     @Autowired
-    private StudentRepository studentRepository;
+    StudentRepository studentRepository;
 
-    @GetMapping("/students")
-    public Page<Student> getAllStudents(Pageable pageable) {
-        return studentRepository.findAll(pageable);
+    @Autowired
+    ClassRepository classRepository;
+
+    @GetMapping("/students/{studentId}")
+    public Student retrieveStudent(@PathVariable long studentId) {
+        Optional<Student> student = studentRepository.findById(studentId);
+        if (!student.isPresent())
+            throw new ResourceNotFoundException("id-" + studentId);
+        return student.get();
     }
 
-    @PostMapping("/students")
-    public Student createStudent(@Valid @RequestBody Student student) {
-        return studentRepository.save(student);
+    @GetMapping("/students")
+    public ResponseEntity getAllStudents() {
+        List<Student> studentList = studentRepository.findAll();
+        return ResponseEntity.ok().body(studentList);
+    }
+
+    @PostMapping("/student")
+    public ResponseEntity addClass(@RequestBody Student student) {
+        return ResponseEntity.ok().body(studentRepository.save(student));
+    }
+
+    @PutMapping("/students/{studentId}")
+    public ResponseEntity<Object> updateStudent(@RequestBody Student student, @PathVariable long studentId) {
+        Optional<Student> studentOptional = studentRepository.findById(studentId);
+        if (!studentOptional.isPresent())
+            return ResponseEntity.notFound().build();
+        student.setStudentId(studentId);
+        studentRepository.save(student);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/students/{studentId}")
+    public void deleteStudent(@PathVariable long studentId) {
+        studentRepository.deleteById(studentId);
+    }
+
+    @GetMapping("/classes")
+    public ResponseEntity getAllClasses() {
+        List<Classes> classesList = classRepository.findAll();
+        return ResponseEntity.ok().body(classesList);
+    }
+
+    @GetMapping("/class")
+    public ResponseEntity searchForClass(@RequestParam(name = "className") String className) {
+        return ResponseEntity.status(HttpStatus.OK).body(classRepository.findAllByName(className));
+    }
+
+    @PostMapping("/class")
+    public ResponseEntity addClass(@RequestBody Classes newClass) {
+        return ResponseEntity.ok().body(classRepository.save(newClass));
+    }
+
+    @PutMapping("/student/class/{classId}")
+    public ResponseEntity enrollStudentInClass(@PathVariable Long classId, @RequestBody Student student) {
+        Optional<Classes> optionalClasses = classRepository.findById(classId);
+        if (optionalClasses.isPresent()) {
+            Classes classes = optionalClasses.get();
+            List<Student> studentList = (List<Student>) classes.getStudents();
+            studentList.add(student);
+            classes.setStudents(studentList);
+            classRepository.save(classes);
+            return ResponseEntity.ok().body("Student enrolled successfully");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/student/{studentId}/class/{classId}")
+    public ResponseEntity cancelEnrollmentOfStudent(@PathVariable Long studentId, @PathVariable Long classId) {
+        Optional<Student> optionalStudent = studentRepository.findById(studentId);
+        Optional<Classes> optionalClasses = classRepository.findById(classId);
+        if (optionalStudent.isPresent() && optionalClasses.isPresent()) {
+            if (optionalClasses.get().getStudents().contains(optionalStudent.get())) {
+                optionalClasses.get().getStudents().remove(optionalStudent.get());
+                classRepository.save(optionalClasses.get());
+                return ResponseEntity.ok().body("Student enrollment cancelled");
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- src/main/java/com/student/info/sys/repository/ClassRepository.java
+++ src/main/java/com/student/info/sys/repository/ClassRepository.java
@@ -4,11 +4,14 @@ import com.student.info.sys.model.Classes;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.management.openmbean.OpenDataException;
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 /**
  * Created by Created by Saeed Murrad on 30/09/19.
  */
 @Repository
 public interface ClassRepository extends JpaRepository<Classes, Long> {
-    Classes findByStudentId(Long studentId);
-
+    Optional<Classes> findAllByName(String className);
 }
